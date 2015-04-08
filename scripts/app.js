58ca9ab7f1022ae5555b8d84434c1f449db065e4
Hiroki Kihara <fj8550jf@aa.jp.fujitsu.com>
2015-04-08 09:46:37 +0900

    jsの静的解析用のrcを追加

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,21 @@
+/*jshint latedef: nofunc */
 'use strict';
 
 angular.module('app', [
-    'app.top',
-    'ngNewRouter'
-])
-  .controller('AppController', ['$router', AppController]);
+  'app.top',
+  'ngNewRouter'
+  ])
+.controller('AppController', ['$router', AppController]);
 
 function AppController ($router) {
   $router.config([
-  	{ path: '/',       redirectTo: '/top' },
-    { path: '/top',     component: 'top' },
-    { path: '/outline', component: 'outline' },
-    { path: '/work',    component: 'work' },
-    { path: '/product', component: 'product' },
-    { path: '/recruit', component: 'recruit' },
-    { path: '/staff',   component: 'staff' },
-    { path: '/access',  component: 'access' }
-  ]);
+    {path: '/',       redirectTo: '/top'},
+    {path: '/top',     component: 'top'},
+    {path: '/outline', component: 'outline'},
+    {path: '/work',    component: 'work'},
+    {path: '/product', component: 'product'},
+    {path: '/recruit', component: 'recruit'},
+    {path: '/staff',   component: 'staff'},
+    {path: '/access',  component: 'access'}
+    ]);
 }
